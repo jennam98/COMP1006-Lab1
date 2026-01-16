992d5c93401204d4ff6bcb3b94796c9e58ea768c
Jenna Moss <jenna.moss@hotmail.com>
2026-01-15 20:52:13 -0500

    Step 3: create instance of car and echo to browser

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+
+require "car.php"; // Including car.php file
+
+
+
+// Creating new car object
+
+$myCar = new Car("Honda", "Civic", 2012);
+
+echo "<p> My Car Information:.</p>";
+echo $myCar->getCar();  // Displaying car information in browser
+
